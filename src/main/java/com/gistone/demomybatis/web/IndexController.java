5b6b027a09d3f1775889d08139979ca730380a2e
panzetao <panzetao@gistone.cn>
2021-04-30 16:56:01 +0800

    修复mapping错误

--- src/main/java/com/gistone/demomybatis/web/IndexController.java
+++ src/main/java/com/gistone/demomybatis/web/IndexController.java
@@ -5,10 +5,12 @@ import com.gistone.demomybatis.database.Student;
 import com.gistone.demomybatis.web.vo.InsertResult;
 import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class IndexController {
 
     private final StudentService studentService;
