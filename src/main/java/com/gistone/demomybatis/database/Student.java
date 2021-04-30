5f42d01b554c63c92bb3cb54a38a1039cfbcc184
panzetao <panzetao@gistone.cn>
2021-04-30 16:20:49 +0800

    随机增加一条数据

--- src/main/java/com/gistone/demomybatis/database/Student.java
+++ src/main/java/com/gistone/demomybatis/database/Student.java
@@ -1,8 +1,10 @@
 package com.gistone.demomybatis.database;
 
+import com.gistone.demomybatis.Util;
 import lombok.Data;
 
-import java.io.Serializable;
+import java.time.Year;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * student
@@ -10,9 +12,21 @@ import java.io.Serializable;
  * @author
  */
 @Data
-public class Student implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class Student {
     private Long id;
     private String name;
     private Integer birth;
+
+    public Student(String name, Integer birth) {
+        this.name = name;
+        this.birth = birth;
+    }
+
+    public static Student randOne() {
+        String name = Util.randName();
+        int age = ThreadLocalRandom.current().nextInt(0, 100);
+        int thisYear = Year.now().getValue();
+        Student student = new Student(name, thisYear - age);
+        return student;
+    }
 }
--- src/main/java/com/gistone/demomybatis/database/StudentDao.java
+++ src/main/java/com/gistone/demomybatis/database/StudentDao.java
@@ -1,5 +1,8 @@
 package com.gistone.demomybatis.database;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface StudentDao {
     int insert(Student record);
 
