ac3498bf7915b3f7fd08e376b41d3b4899e45b84
panzetao <panzetao@gistone.cn>
2021-05-07 09:14:56 +0800

    sort测试

---
+++ src/test/java/com/gistone/demomybatis/SpeadTest.java
@@ -0,0 +1,38 @@
+package com.gistone.demomybatis;
+
+import org.springframework.util.StopWatch;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SpeadTest {
+    public static void main(String[] args) {
+
+        int ARRAY_SIZE = 50000;
+        int[] data = new int[ARRAY_SIZE];
+        int DATA_STRIDE = 256;
+
+        for (int c = 0; c < ARRAY_SIZE; ++c) {
+            data[c] = ThreadLocalRandom.current().nextInt() % DATA_STRIDE;
+        }
+
+        Arrays.sort(data); // 7.56 s
+
+
+        {  // 测试部分
+            StopWatch stopWatch = new StopWatch();
+            long sum = 0;
+            stopWatch.start();
+            for (int i = 0; i < 100000; ++i) {
+                for (int c = 0; c < ARRAY_SIZE; ++c) {
+                    if (data[c] >= 128) sum += data[c];
+                }
+            }
+            stopWatch.stop();
+            double elapsedTime = stopWatch.getTotalTimeSeconds();
+
+            System.out.println(elapsedTime);
+            System.out.println("sum = " + sum);
+        }
+    }
+}
